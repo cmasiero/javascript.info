b408a18db3c4cabe6964d906f524b2323b3c062e
c.masiero <masiero.cristiano@consoft.it>
2018-10-29 12:22:56 +0100

    Change-Id: I0000000000000000000000000000000000000000
    commit
    
    Change-Id: I6387208a3ac9411d3fd0b1d5d92881e46280502d

--- WebContent/011-object-to-primitive-conversion.js
+++ WebContent/011-object-to-primitive-conversion.js
@@ -5,6 +5,20 @@ function object_to_primitive_conversion(){
 	
 	console.log("*** object_to_primitive_conversion() ***");
 	
+	alert("ciao");
+	alert("ciao");
+	alert("ciao");
+	alert("ciao");
+	alert("ciao");
+	alert("ciao");
+	alert("ccc");
+	alert("ccc");
+	alert("ccc");
+	alert("xxx");
+	alert("xxx");
+	alert("xxx");
+	alert("xxx");
+	
 	
 	
 }
